234a50a7766fa4eae4dee4beb89502a26f399a17
Tofuxx001 <85976632+Tofuxx001@users.noreply.github.com>
2023-11-03 22:56:50 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import CountriesList from './components/CountriesList';
+
+function App() {
+  return (
+    <div className="App">
+      <CountriesList />
+    </div>
+  );
+}
+
+export default App;
